2d17cf313276e33bdeb743dc6ec24bb6a6c524fd
AdhityaAwr <145439759+AdhityaAwr@users.noreply.github.com>
2023-10-16 12:07:47 +0700

    Add files via upload
    tugas1

---
+++ Tugs 1/string.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Membuat 3 variabel string
+  String string1 = "Adhitya ";
+  String string2 = "Arya ";
+  String string3 = "Wiratama R";
+
+  // Menggabungkan ketiga string
+  String hasilGabungan = string1 + string2 + string3;
+
+  // Menampilkan hasilnya di layar
+  print(hasilGabungan);
+}
